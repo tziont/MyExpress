59543e8252295b5449c4fb7e8969c72fc0084cea
Tzion Tzur <ttzur@aligntech.com>
2019-08-22 14:41:07 +0300

    starter point

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger');
+
+const app = express();
+
+
+
+//Init middleware
+ //app.use(logger);
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Member API Routes
+app.use('/api/members', require('./routes/api/members'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
