407c0dac89e1578b5d3e7a8582bde1320edb689a
Tzion Tzur <tzion800@gmail.com>
2019-08-24 21:21:32 +0300

    progress in express

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const uuid = require('uuid');
 const router = express.Router();
 const members = require('../../Members');
 
@@ -15,4 +16,34 @@ router.get('/:id', (req, res) =>{
     }
 });
 
+// Create Member
+router.post('/', (req, res) => {
+    const newMember = {
+            id: uuid.v4(),
+            name: req.body.name,
+            email: req.body.email,
+            status: 'active'
+        }
+    if(!newMember.name || !newMember.email) {
+       return res.status(400).json({msg: 'Please include a name and enail'});
+    }
+    members.push(newMember);
+    res.json(members);
+});
+
+//Update Member
+router.put('/:id', (req, res) =>{
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if(found){
+    const updateMember = req.body;
+    members.forEach(member => {
+        if(member.id === parseInt(req.params.id)){
+            member.name = updateMember.name ? updateMember.name : member.name;
+            member.email = pdateMember.email ? updateMember.email : member.email;
+        }
+    });
+    }else{
+        res.status(400).json({msg:`No member with the id of ${req.params.id}`});
+    }
+});
 module.exports = router;
